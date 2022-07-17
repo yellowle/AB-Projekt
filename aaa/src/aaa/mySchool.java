0c5586845b955ebdb9280ec850686e36bcecd55d
yellowle <gelbeleopard@gmail.com>
2022-07-17 20:49:41 +0200

    Kommentar hinzügefügt

--- aaa/src/aaa/mySchool.java
+++ aaa/src/aaa/mySchool.java
@@ -2,6 +2,10 @@ package aaa;
 import java.util.Scanner;
 public class mySchool {
 
+	
+	
+	// komment für das Programm
+	
 	public static void main(String[] args) {
 		
 		Scanner myscanner = new Scanner(System.in);
