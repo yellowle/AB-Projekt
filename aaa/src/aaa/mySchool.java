641ba1507c237d1a7676ae32a59ab143d7cddd68
yellowle <gelbeleopard@gmail.com>
2022-07-17 20:22:24 +0200

    commit mit commit

--- aaa/src/aaa/mySchool.java
+++ aaa/src/aaa/mySchool.java
@@ -5,7 +5,7 @@ public class mySchool {
 	public static void main(String[] args) {
 		
 		Scanner myscanner = new Scanner(System.in);
-		System.out.println("sag mal bitte , wo du studiert hast !!!");
+		System.out.println("sag mal bitte , wo du studiert hast !!");
 		String schule = myscanner.nextLine();
 		
 		System.out.println("Du warst in "+schule);
