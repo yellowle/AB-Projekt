41373aec4992c188e1a67a489fed4330dd9b74bd
yellowle <gelbeleopard@gmail.com>
2022-07-17 19:45:52 +0200

    Scanner

---
+++ aaa/src/aaa/mySchool.java
@@ -0,0 +1,17 @@
+package aaa;
+import java.util.Scanner;
+public class mySchool {
+
+	public static void main(String[] args) {
+		
+		Scanner myscanner = new Scanner(System.in);
+		System.out.println("Wie heisst dein Schule?");
+		String schule = myscanner.nextLine();
+		
+		System.out.println("Du warst in "+schule);
+		
+		
+
+	}
+
+}
